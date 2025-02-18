835b58282b5b2588e0960f8047aa7e875f59f69e
horse-3903 <chongchoonhourafael@gmail.com>
2025-02-18 19:23:59 +0800

    v0.2.0

--- src/main.py
+++ src/main.py
@@ -1,40 +1,7 @@
-import os
-from dotenv import load_dotenv
+from google import genai
 
-from groq import Groq
-import base64
-
-load_dotenv()
-
-# Function to encode the image
-def encode_image(image_path):
-  with open(image_path, "rb") as image_file:
-    return base64.b64encode(image_file.read()).decode('utf-8')
-
-
-image_path = "data/apple/201_ZT85G.jpg"
-base64_image = encode_image(image_path)
-
-client = Groq(
-    api_key=os.environ.get("GROQ_API_KEY"),
+client = genai.Client(api_key="YOUR_API_KEY")
+response = client.models.generate_content(
+    model="gemini-2.0-flash", contents="Explain how AI works"
 )
-
-chat_completion = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": [
-                {"type": "text", "text": "Reply what is in the CAPTCHA provided below, with only the letters",},
-                {
-                    "type": "image_url",
-                    "image_url": {
-                        "url": f"data:image/jpeg;base64,{base64_image}",
-                    },
-                },
-            ],
-        }
-    ],
-    model="llama-3.2-11b-vision-preview",
-)
-
-print(chat_completion.choices[0].message.content)
+print(response.text)
